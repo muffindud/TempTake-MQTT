3195a31640deb23f154390f2d78a9a79409ec9f6
muffindud <ccatlabuga2002@gmail.com>
2025-01-29 20:13:45 +0200

    reformat project... again...

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM gradle:8.4-jdk21 AS build
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN gradle build --no-daemon
+RUN ./gradlew jar
 
 FROM openjdk:21-jdk-slim
 RUN mkdir /app
 COPY --from=build /home/gradle/src/build/libs/*.jar /app/app.jar
-ENTRYPOINT ["java", "-cp", "/app/app.jar", "muffindud.temptake.Main"]
+ENTRYPOINT ["./gradlew", "run"]
---
+++ app/src/main/java/muffindud/temptake/App.java
@@ -0,0 +1,39 @@
+package muffindud.temptake;
+
+import org.eclipse.paho.client.mqttv3.*;
+
+public class App {
+    //    private static final String BROKER =
+//            "tcp://" + System.getenv("MQTT_BROKER") + ":" + System.getenv("MQTT_PORT");
+    private static final String BROKER = "tcp://192.168.0.23:1883";
+    private static final String MQTT_TOPIC = "temptake/manager";
+    private static final String MQTT_CLIENT_ID = "temptake_consumer";
+
+    public static void main(String[] args) {
+        try {
+            MqttClient client = new MqttClient(BROKER, MQTT_CLIENT_ID);
+            MqttConnectOptions options = new MqttConnectOptions();
+
+            options.setAutomaticReconnect(true);
+            options.setCleanSession(true);
+            options.setConnectionTimeout(10);
+
+            client.connect(options);
+            System.out.println("Connected to MQTT broker");
+
+            client.subscribe(MQTT_TOPIC, (topic, message) -> {
+                // print raw payload in hex
+                byte[] payload = message.getPayload();
+                StringBuilder hexPayload = new StringBuilder();
+                for (byte b : payload) {
+                    hexPayload.append(String.format("%02X ", b));
+                }
+                System.out.println("Received message: " + hexPayload.toString());
+
+                // TODO: Insert into database
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+}
